951cb3f5857674a0bdd58a88b49783c711cfa193
Manimaran Selvan <mselvan@belyf.com>
2020-04-18 21:45:24 +0530

    New data
    With extra columns.

--- app/services/data-service.js
+++ app/services/data-service.js
@@ -12,13 +12,36 @@ const DataService = {
             {
                 $project: {
                     _id: 0,
-                    date: {$dateToString: { format: "%Y-%m-%d", date: "$lastTradedDate" }},
-                    close: {$round: [{$toDouble : "$close"}, 2]},
-                    week_low: {$round: [{$toDouble : "$week_low"}, 2]}
+                    date: {$dateToString: {format: "%Y-%m-%d", date: "$lastTradedDate"}},
+                    open: {$round: [{$toDouble: "$open"}, 2]},
+                    high: {$round: [{$toDouble: "$high"}, 2]},
+                    low: {$round: [{$toDouble: "$low"}, 2]},
+                    close: {$round: [{$toDouble: "$close"}, 2]},
+                    week_low: {$round: [{$toDouble: "$week_low"}, 2]},
+                    week_high: {$round: [{$toDouble: "$week_high"}, 2]},
+                    month_low: {$round: [{$toDouble: "$month_low"}, 2]},
+                    month_high: {$round: [{$toDouble: "$month_high"}, 2]},
+                    quarter_low: {$round: [{$toDouble: "$quarter_low"}, 2]},
+                    quarter_high: {$round: [{$toDouble: "$quarter_high"}, 2]},
+                    year_low: {$round: [{$toDouble: "$year_low"}, 2]},
+                    year_high: {$round: [{$toDouble: "$year_high"}, 2]},
+                    five_year_low: {$round: [{$toDouble: "$five_year_low"}, 2]},
+                    five_year_high: {$round: [{$toDouble: "$five_year_high"}, 2]},
+                    week_low_percent_change: {$toDouble: {$divide: [{$subtract: ["$close", "$week_low"]}, "$close"]}},
+                    week_high_percent_change: {$toDouble: {$divide: [{$subtract: ["$close", "$week_high"]}, "$close"]}},
+                    month_low_percent_change: {$toDouble: {$divide: [{$subtract: ["$close", "$month_low"]}, "$close"]}},
+                    month_high_percent_change: {$toDouble: {$divide: [{$subtract: ["$close", "$month_high"]}, "$close"]}},
+                    quarter_low_percent_change: {$toDouble: {$divide: [{$subtract: ["$close", "$quarter_low"]}, "$close"]}},
+                    quarter_high_percent_change: {$toDouble: {$divide: [{$subtract: ["$close", "$quarter_high"]}, "$close"]}},
+                    year_low_percent_change: {$toDouble: {$divide: [{$subtract: ["$close", "$year_low"]}, "$close"]}},
+                    year_high_percent_change: {$toDouble: {$divide: [{$subtract: ["$close", "$year_high"]}, "$close"]}},
+                    five_year_low_percent_change: {$toDouble: {$divide: [{$subtract: ["$close", "$five_year_low"]}, "$close"]}},
+                    five_year_high_percent_change: {$toDouble: {$divide: [{$subtract: ["$close", "$five_year_high"]}, "$close"]}}
                 }
             }
         ]).toArray();
-        let response = {ticker: ticker, timeseries: result};
+        let response = {};
+        response[ticker] = result;
         return response;
     }
 };
