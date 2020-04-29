4a6fb2353a085dfeb649e24e24b8717c7e405b5b
Kalaipriya Selvan <kalaipriya@belyf.com>
2020-04-29 09:57:45 +0530

    Full chart before tweaks

--- charts/capm.py
+++ charts/capm.py
@@ -10,60 +10,72 @@ ef = pd.read_csv("data/ef.csv")
 
 data = [
     go.Scatter(
-        x=portfolio['SD'],
-        y=portfolio['Returns'],
-        name='Portfolio',
-        text=portfolio['Ticker'],
-        marker=dict(size=6),
-        mode='markers'
+        x=[0, 0.119],
+        y=[0.02, 0.18],
+        name="CML",
+        text="CML",
+        mode="lines",
+        line=dict(color="Blue")
     ),
     go.Scatter(
         x=ef['x'],
         y=ef['y'],
         name="Efficient frontier",
         text="EF",
-        line_shape="spline"
+        line_shape="spline",
+        mode="lines",
+        line=dict(color="Red")
     )
 ]
 
+for index, row in portfolio.iterrows():
+    data.append(go.Scatter(
+        x=[row["SD"]],
+        y=[row["Returns"]],
+        mode='markers',
+        name=row["Ticker"],
+        text=[row["Ticker"]]
+    ))
+
 fig = go.Figure(data=data)
-# fig.add_shape(
-#     # Line Diagonal
-#     type="line",
-#     x0=0,
-#     y0=0.116,
-#     x1=1,
-#     y1=0.116,
-#     line=dict(
-#         color="Gray",
-#         width=2,
-#         dash="dash",
-#     )
-# )
-#
-# fig.add_shape(
-#     # Line Diagonal
-#     type="line",
-#     x0=1,
-#     y0=0,
-#     x1=1,
-#     y1=0.116,
-#     line=dict(
-#         color="Gray",
-#         width=2,
-#         dash="dash",
-#     )
-# )
+
+fig.add_shape(
+    type="line",
+    x0=0.0601,
+    y0=0,
+    x1=0.0601,
+    y1=0.0894,
+    line=dict(
+        color="Gray",
+        width=1,
+        dash="dash",
+    )
+)
+
+fig.add_shape(
+    # Line Diagonal
+    type="line",
+    x0=0.0601,
+    y0=0.0894,
+    x1=0,
+    y1=0.0894,
+    line=dict(
+        color="Gray",
+        width=1,
+        dash="dash",
+    )
+)
 
 fig.update_layout(
     xaxis=dict(
-        range=[0, 0.2],
+        range=[0, 0.14],
         spikemode='toaxis'
     ),
     yaxis=dict(
-        range=[0, 0.2],
+        range=[0, 0.18],
         spikemode='toaxis'
     ),
+    width=1200,
     height=700)
 
 app = dash.Dash()
