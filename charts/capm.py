913c535236ff35d1b73d49b48a098ef7a2ec3b66
Kalaipriya Selvan <kalaipriya@belyf.com>
2020-04-29 11:12:36 +0530

    Minor color and size change

--- charts/capm.py
+++ charts/capm.py
@@ -4,9 +4,9 @@ import dash_html_components as html
 import pandas as pd
 import plotly.graph_objects as go
 
-sml = pd.read_csv("data/sml.csv")
 portfolio = pd.read_csv("data/portfolio.csv")
 ef = pd.read_csv("data/ef.csv")
+colors = ["#d90074", "#592400", "#95e639", "#0d3033", "#6559b3", "#330014", "#e50000", "#7f5940", "#398020", "#79daf2", "#440080", "#ff0044", "#d90000", "#ff8800", "#a7cc99", "#406a80", "#ee00ff", "#f2b6c6", "#7f0000", "#7f4400", "#003307", "#80c4ff", "#912699"]
 
 data = [
     go.Scatter(
@@ -34,7 +34,9 @@ for index, row in portfolio.iterrows():
         y=[row["Returns"]],
         mode='markers',
         name=row["Ticker"],
-        text=[row["Ticker"]]
+        text=[row["Ticker"]],
+        line=dict(color=colors[index]),
+        marker=dict(size=8)
     ))
 
 fig = go.Figure(data=data)
