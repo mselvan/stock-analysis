d2e4d92de3a2ffa1f531f1401fd7bcb0aaefdf8b
Manimaran Selvan <mselvan@belyf.com>
2020-05-03 17:52:50 +0530

    Sector charts

---
+++ charts/sector-ef-cml.py
@@ -0,0 +1,93 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.graph_objects as go
+
+portfolio = pd.read_csv("data/portfolio.csv")
+s_portfolio = pd.read_csv("data/portfolio-sector-wise.csv")
+colors = ["#d90074", "#592400", "#95e639", "#0d3033", "#6559b3", "#330014", "#e50000", "#7f5940", "#398020", "#79daf2", "#440080", "#ff0044", "#d90000", "#ff8800", "#a7cc99", "#406a80", "#ee00ff", "#f2b6c6", "#7f0000", "#7f4400", "#003307", "#80c4ff", "#912699"]
+
+data = [
+    go.Scatter(
+        x=[0, 0.125],
+        y=[0, 0.192],
+        name="CML",
+        text="CML",
+        mode="lines",
+        line=dict(color="Blue")
+    ),
+    go.Scatter(
+        x=[0.09, 0.06, 0.07],
+        y=[0.135, 0.0825, 0.06],
+        name="Efficient frontier",
+        text="EF",
+        line_shape="spline",
+        mode="lines",
+        line=dict(color="Red")
+    )
+]
+
+for index, row in s_portfolio.iterrows():
+    data.append(go.Scatter(
+        x=[row["SD"]],
+        y=[row["Returns"]],
+        mode='markers',
+        name=row["Sector"],
+        text=[row["Sector"]],
+        line=dict(color=colors[index]),
+        marker=dict(size=8)
+    ))
+
+fig = go.Figure(data=data)
+
+fig.add_shape(
+    type="line",
+    x0=0.07790,
+    y0=0,
+    x1=0.07790,
+    y1=0.11600,
+    line=dict(
+        color="Gray",
+        width=1,
+        dash="dash",
+    )
+)
+
+fig.add_shape(
+    type="line",
+    x0=0.07790,
+    y0=0.11600,
+    x1=0,
+    y1=0.11600,
+    line=dict(
+        color="Gray",
+        width=1,
+        dash="dash",
+    )
+)
+
+fig.update_layout(
+    xaxis=dict(
+        range=[0, 0.14],
+        spikemode='toaxis',
+        title="Risk"
+    ),
+    yaxis=dict(
+        range=[0, 0.18],
+        spikemode='toaxis',
+        title='Returns'
+
+    ),
+    width=1200,
+    height=700,
+    plot_bgcolor="rgb(232, 232, 232)",
+    legend_orientation="h"
+)
+
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+
+app.run_server(debug=True, use_reloader=True)
